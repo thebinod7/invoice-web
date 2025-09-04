c221152fee1991f9c04cae6779b2061e868eb117
Binod Chaudhary <thebinod7@gmail.com>
2025-09-04 08:27:55 +0545

    removed adblocker popup info

--- app/thanks/page.tsx
+++ app/thanks/page.tsx
@@ -1,39 +1,20 @@
 'use client';
-import { AdblockDetector } from 'adblock-detector';
-import { useEffect, useState } from 'react';
 import AdBanner from '../components/AdBanner';
-import AdBlockAlert from '../components/AdBlockAlert';
 import TopBannerAd from '../components/Paid/TopBannerAd';
 import { GOOGLE_AD } from '../constants';
+import { useGetActiveAd } from '../hooks/backend/invoice';
 import CTA from './CTA';
 import InvoiceGenSuccess from './InvoiceGenSuccess';
 import InvoiceSavedInfo from './InvoiceSavedInfo';
-import { useGetActiveAd } from '../hooks/backend/invoice';
 
 export default function ThankYouPage() {
-  const adbDetector = new AdblockDetector(); // call
-  const userHasAdblock = adbDetector.detect();
-  const [showAdBlockAlert, setShowAdBlockAlert] = useState(false);
-
   const { data } = useGetActiveAd();
 
   const activeAd = data?.data?.result || null;
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      if (userHasAdblock) {
-        setShowAdBlockAlert(true);
-      }
-    }, 3000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  console.log({ userHasAdblock });
-
   return (
     <>
-      {showAdBlockAlert && <AdBlockAlert />}
+      {/* {showAdBlockAlert && <AdBlockAlert />} */}
       <div className="bg-gradient-to-br from-green-50 to-emerald-50 flex items-center justify-center p-4">
         <div className="max-w-md w-full">
           {/* <div className="text-blue-500 underline text-right">
