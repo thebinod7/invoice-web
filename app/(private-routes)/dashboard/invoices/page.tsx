e432cf1cade37a861bfd7663cfa32833705db5dc
Binod Chaudhary <thebinod7@gmail.com>
2025-12-21 21:07:51 +0545

    added debounce search

--- app/(private-routes)/dashboard/invoices/page.tsx
+++ app/(private-routes)/dashboard/invoices/page.tsx
@@ -2,28 +2,36 @@
 import { invoiceColumns } from '@/app/components/Invoice/invoice.columns';
 import { PAZE_SIZE } from '@/app/constants';
 import { useListMyInvoices } from '@/app/hooks/backend/invoice.hook';
+import { useDebounce } from '@/app/hooks/ui/debounce';
 import { TanstackTable } from '@/ui/TanstackTable';
 import { useEffect, useMemo, useState } from 'react';
+
+const DEBOUNCE_DELAY = 700;
 
 export default function page() {
   const columns = useMemo(() => invoiceColumns(), []);
 
   const [currentPage, setCurrentPage] = useState(1);
+  const [search, setSearch] = useState('');
+  const debouncedSearch = useDebounce(search, DEBOUNCE_DELAY);
   const { data, isLoading, refetch } = useListMyInvoices({
     page: currentPage,
     perPage: PAZE_SIZE,
+    search: debouncedSearch,
   });
   const result = data?.data?.result || null;
 
   useEffect(() => {
     refetch();
-  }, [currentPage]);
+  }, [currentPage, search]);
 
   return (
     <div className="px-8">
       <TanstackTable
         columns={columns}
         data={result?.rows || []}
+        globalFilter={search}
+        onGlobalFilterChange={setSearch}
         searchKey="invoiceNumber"
         searchPlaceholder="Search by invoice number..."
         isLoading={isLoading}
