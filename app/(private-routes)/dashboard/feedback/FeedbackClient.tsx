d35794f38f263b6e3a9a556c214ad40a980c2c03
Binod Chaudhary <thebinod7@gmail.com>
2025-12-26 13:57:33 +0545

    added feedback form

--- app/(private-routes)/dashboard/feedback/FeedbackClient.tsx
+++ app/(private-routes)/dashboard/feedback/FeedbackClient.tsx
@@ -3,8 +3,9 @@
 import type React from 'react';
 
 import { API_ROUTES } from '@/app/constants/api-routes';
+import { useAuthContext } from '@/app/context/useAuthContext';
 import { sanitizeError } from '@/app/helpers';
-import { patchRequest } from '@/app/helpers/request';
+import { postRequest } from '@/app/helpers/request';
 import { Button } from '@/components/ui/button';
 import { Card } from '@/components/ui/card';
 import { Textarea } from '@/components/ui/textarea';
@@ -12,19 +13,25 @@ import { useMutation } from '@tanstack/react-query';
 import { useState } from 'react';
 import { toast } from 'sonner';
 
+const MSG_LIMIT = 500;
+
 export default function FeedbackClient() {
+  const { currentUser } = useAuthContext();
+  const [submitted, setSubmitted] = useState(false);
   const [formData, setFormData] = useState({
     message: '',
   });
 
   const sendFeedbackMutation = useMutation({
     mutationFn: (payload: any) => {
-      return patchRequest(`${API_ROUTES.USERS}/me`, payload);
+      return postRequest(`${API_ROUTES.APP}/feedback`, payload);
     },
     onError: (error) => {
       toast.error(sanitizeError(error));
     },
     onSuccess: () => {
+      setSubmitted(true);
+      setFormData({ message: '' });
       toast.success(
         'Thank you for your feedback! We will get back to you soon.'
       );
@@ -34,8 +41,11 @@ export default function FeedbackClient() {
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     const payload = {
+      email: currentUser?.email,
       message: formData.message,
     };
+    if (payload.message.length > MSG_LIMIT)
+      return toast.error('Message too long!');
     return sendFeedbackMutation.mutate(payload);
   };
 
@@ -57,6 +67,7 @@ export default function FeedbackClient() {
             <Textarea
               required={true}
               rows={10}
+              value={formData.message}
               placeholder="Type your message here. "
               onChange={(e) =>
                 setFormData({
@@ -67,13 +78,28 @@ export default function FeedbackClient() {
             />
           </div>
 
-          <Button
-            disabled={sendFeedbackMutation.isPending}
-            type="submit"
-            className="w-full h-10 mt-8"
-          >
-            {sendFeedbackMutation.isPending ? 'Sending...' : 'Send Message'}
-          </Button>
+          <p className="text-xs text-muted-foreground">
+            Max character limit: {formData.message.length}/{MSG_LIMIT}
+          </p>
+
+          {submitted ? (
+            <Button
+              disabled={true}
+              type="button"
+              variant={'secondary'}
+              className="w-full h-10 mt-8"
+            >
+              Feedback Sent
+            </Button>
+          ) : (
+            <Button
+              disabled={sendFeedbackMutation.isPending}
+              type="submit"
+              className="w-full h-10 mt-8"
+            >
+              {sendFeedbackMutation.isPending ? 'Sending...' : 'Send Message'}
+            </Button>
+          )}
         </form>
       </div>
     </Card>
