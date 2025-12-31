12008909a36531b491f5896e7b483ed429096917
Binod Chaudhary <thebinod7@gmail.com>
2025-12-31 14:54:35 +0545

    added google signup

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import GoogleLogin from '@/ui/GoogleLogin';
 import { useMutation } from '@tanstack/react-query';
 import {
   AlertCircle,
@@ -11,12 +12,11 @@ import {
 } from 'lucide-react';
 import Link from 'next/link';
 import { useState, type FormEvent } from 'react';
+import { toast } from 'sonner';
+import { APP_PATHS } from '../constants';
 import { API_ROUTES } from '../constants/api-routes';
 import { emailValidator, sanitizeError } from '../helpers';
 import { postRequest } from '../helpers/request';
-import { APP_PATHS } from '../constants';
-import { toast } from 'sonner';
-import { useGoogleLogin } from '@react-oauth/google';
 
 export default function MagicLinkLogin() {
   const [email, setEmail] = useState('');
@@ -50,29 +50,6 @@ export default function MagicLinkLogin() {
     setErrorMessage('');
     return useSendMagicLinkMutation.mutateAsync({ email });
   };
-
-  const loginWithGoogleMutation = useMutation({
-    mutationFn: (payload: any) => {
-      return postRequest(API_ROUTES.AUTH + '/google-login', payload);
-    },
-    onError: (err) => {
-      console.error('Google login error:', err);
-      toast.error('Failed to login with Google, Please try again.');
-    },
-    onSuccess: ({ data }) => {
-      toast.success('Successfully logged in with Google!');
-      window.location.replace(APP_PATHS.DASHBOARD.HOME);
-    },
-  });
-
-  const googleLogin = useGoogleLogin({
-    onSuccess: (response: any) => {
-      return loginWithGoogleMutation.mutateAsync({
-        googleToken: response.access_token,
-      });
-    },
-    onError: () => toast.error('Failed to login with Google!'),
-  });
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-white px-4">
@@ -175,34 +152,7 @@ export default function MagicLinkLogin() {
               )}
             </button>
 
-            <button
-              onClick={() => googleLogin()}
-              type="button"
-              className="group cursor-pointer relative w-full bg-white border border-gray-200  rounded-2xl py-3 px-4 text-gray-700 font-medium flex items-center justify-center space-x-3 transition-all duration-200"
-            >
-              <div className="flex items-center justify-center w-5 h-5">
-                <svg viewBox="0 0 24 24" className="w-5 h-5">
-                  <path
-                    fill="#4285F4"
-                    d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
-                  />
-                  <path
-                    fill="#34A853"
-                    d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
-                  />
-                  <path
-                    fill="#FBBC05"
-                    d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
-                  />
-                  <path
-                    fill="#EA4335"
-                    d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
-                  />
-                </svg>
-              </div>
-              <span>Continue with Google</span>
-              <div className="absolute inset-0 rounded-2xl bg-gradient-to-r from-blue-600/0 via-blue-600/0 to-purple-600/0 transition-all duration-300"></div>
-            </button>
+            <GoogleLogin btnText="Login with Google" />
 
             <div className="flex justify-center">
               <Link
